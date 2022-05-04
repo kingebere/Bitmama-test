cf120a9927853b03b88bb3442f6680fab9e9f582
Dike  Goodluck <Gudoski30@gmail.com>
2022-05-04 18:57:06 +0100

    fix env

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,7 +5,6 @@ import {useSelector} from "react-redux";
 
 const Login = () => {
   const navigate = useNavigate();
-  const redirect_uri = process.env.REACT_APP_REDIRECT_URI;
 
   const {post} = useSelector(state => ({
     ...state.app,
@@ -21,7 +20,7 @@ const Login = () => {
     <>
       <button className="login-container btn ">
         <a
-          href={`https://github.com/login/oauth/authorize?scope=user&client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${redirect_uri}`}
+          href={`https://github.com/login/oauth/authorize?scope=user&client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}`}
         >
           <p className="Login__btn">Login with GitHub</p>
         </a>
