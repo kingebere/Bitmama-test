4447405bc367d39c1b3d33e6095d8313051b1088
Dike  Goodluck <Gudoski30@gmail.com>
2022-05-04 21:37:01 +0100

    add k

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,14 +1,11 @@
 import React, {useEffect} from "react";
 import {useNavigate} from "react-router-dom";
 
-import {useDispatch, useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import "./LoginPage.css";
 import Login from "../../components/Login/Login";
-import {getUser} from "../../redux/features/postSlice";
 function LoginPage() {
   const navigate = useNavigate();
-
-  const dispatch = useDispatch();
 
   const {post} = useSelector(state => ({
     ...state.app,
@@ -16,17 +13,6 @@ function LoginPage() {
   useEffect(() => {
     if (post.length > 0) {
       navigate("/");
-    }
-    // After requesting Github access, Github redirects back to your app with a code parameter
-    const url = window.location.href;
-    const hasCode = url.includes("?code=");
-
-    // If Github API returns the code parameter
-    if (hasCode) {
-      const newUrl = url.split("?code=");
-      window.history.pushState({}, null, newUrl[0]);
-      const code = newUrl[1];
-      dispatch(getUser({code}));
     }
   }, []);
 
