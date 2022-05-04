eff864d4816fbc8be2740b589e66306ddb06ff09
Dike  Goodluck <Gudoski30@gmail.com>
2022-05-04 20:40:13 +0100

    add b

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,7 +7,7 @@ import Repo from "../../containers/Repo/Repo";
 import NavBar from "../../components/NavBar/NavBar";
 
 import {useDispatch, useSelector} from "react-redux";
-import {getRepo, getUser} from "../../redux/features/postSlice";
+import {getRepo} from "../../redux/features/postSlice";
 function Home() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -19,19 +19,6 @@ function Home() {
   useEffect(() => {
     if (post.length === 0) {
       navigate("/login");
-    }
-  }, []);
-  useEffect(() => {
-    // After requesting Github access, Github redirects back to your app with a code parameter
-    const url = window.location.href;
-    const hasCode = url.includes("?code=");
-
-    // If Github API returns the code parameter
-    if (hasCode) {
-      const newUrl = url.split("?code=");
-      window.history.pushState({}, null, newUrl[0]);
-      const code = newUrl[1];
-      dispatch(getUser({code}));
     }
   }, []);
 
